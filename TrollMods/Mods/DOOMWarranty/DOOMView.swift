9e4e5013ccb408e224e2642dee11337161ca7516
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-08 18:43:10 -0500

    But can it run DOOM?

---
+++ TrollMods/Mods/DOOMWarranty/DOOMView.swift
@@ -0,0 +1,20 @@
+//
+//  DOOMView.swift
+//  TrollMods
+//
+//  Created by Hariz Shirazi on 2023-01-08.
+//
+
+import SwiftUI
+
+struct DOOMView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DOOMView_Previews: PreviewProvider {
+    static var previews: some View {
+        DOOMView()
+    }
+}
